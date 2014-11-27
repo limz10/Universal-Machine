eb91b408f12692ae8ad7fb384f2b4221d4ffc5c7
Spencer Schoeben <spencer@netspencer.com>
2014-11-26 20:02:38 -0500

    blah blah

---
+++ dyn_array.h
@@ -0,0 +1,19 @@
+#ifndef DYN_ARRAY_H
+#define DYN_ARRAY_H
+
+#include "virtualization.h"
+
+typedef struct Array_T * Array;
+typedef struct Queue_T * Queue;
+
+Queue Queue_new(unsigned hint);
+Array Array_new(unsigned hint);
+
+Word Queue_pop(Queue queue);
+void Queue_push(Queue queue, Word value);
+Word* Array_at(Array array, unsigned pos);
+
+void Queue_free(Queue* seq);
+void Array_free(Array* seq);
+
+#endif // DYN_ARRAY_H
